318bfd11aff2d0494fe2e56e50973e725d24a25b
KelvinAarssen36 <117264110+KelvinAarssen36@users.noreply.github.com>
2025-03-12 10:05:29 +0100

    Spatie install

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Verkoper\VerkoperController;
+use App\Http\Controllers\User\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +22,23 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+
+    // Routes voor algemene gebruikers (user)
+    Route::middleware(['auth', 'role:user'])->group(function () {
+        Route::get('/user/dashboard', [UserController::class, 'dashboard'])->name('user.dashboard');
+
+    });
+
+// Routes voor verkopers
+    Route::middleware(['auth', 'role:verkoper'])->prefix('verkoper')->name('verkoper.')->group(function () {
+        Route::get('/dashboard', [VerkoperController::class, 'dashboard'])->name('dashboard');
+    });
+
+// Routes voor beheerders (admin)
+    Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    });
 });
 
 require __DIR__.'/auth.php';
