d46839c99fab5692cc6e831de0b7687107509b8f
michaelhabieb <k.justmichael@gmail.com>
2025-03-18 09:34:09 +0100

    verkoper sidebar commented

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -32,7 +32,7 @@
             @endrole
 
 
-            @role('admin')
+            {{-- @role('admin')
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Verkopers')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
@@ -52,7 +52,7 @@
                     </flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
-            @endrole
+            @endrole --}}
 
             
 
